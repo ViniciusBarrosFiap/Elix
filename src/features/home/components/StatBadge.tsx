152a1476728b388d0b39990f60029777fc4829df
lucassperanzini <rm98297@fiap.com.br>
2026-08-14 15:56:49 -0300

    design

--- src/features/home/components/StatBadge.tsx
+++ src/features/home/components/StatBadge.tsx
@@ -10,8 +10,8 @@ const StatBadge = ({
   OnPress?: () => void;
 }) => (
   <TouchableOpacity
-    className="flex-row items-center min-h-10 px-3 py-1.5 ml-3 rounded-lg border border-white/10"
-    style={{ backgroundColor: "rgba(255,255,255,0.07)" }}
+    className="flex-row items-center min-h-10 px-3 py-1.5  rounded-lg "
+    // style={{ backgroundColor: "rgba(255,255,255,0.07)" }}
     onPress={OnPress}
   >
     {icon}
