9017c539ba0b4a391f66e4c5e470fc0bf263eb63
lucassperanzini <rm98297@fiap.com.br>
2026-08-27 15:48:01 -0300

    .

---
+++ app/(tabs)/profile/index.tsx
@@ -0,0 +1,4 @@
+// Esta página serve apenas para renderizar o ProfileScreen. Toda a lógica e UI dele estão na url indicada no import.
+import ProfileScreen from "@/src/features/profile/ProfileScreen";
+
+export default ProfileScreen;
